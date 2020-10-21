f0a88f878cf723b04afbdb11be9d6ad0e6c61145
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-10-21 13:01:30 +0300

    Intermediate saving

--- app/src/main/java/ru/avdeev/android/a3_3_2_language_switching_page/MainActivity.java
+++ app/src/main/java/ru/avdeev/android/a3_3_2_language_switching_page/MainActivity.java
@@ -2,13 +2,51 @@ package ru.avdeev.android.a3_3_2_language_switching_page;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner languageSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        languageSpinner=findViewById(R.id.languageSpinner);
+        givLanguage();
     }
+
+    public void onClick(View view){
+        Locale locale = new Locale("ru");
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        recreate();
+    }
+
+    private void givLanguage() {
+        ArrayAdapter<CharSequence> adapterLanguages = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+        adapterLanguages.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapterLanguages);
+
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String [] choiceLanguages = getResources().getStringArray(R.array.languages);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //showMyMessage("Ничего не выбрано", MainActivity.this);
+            }
+        });
+    }
+
 }
